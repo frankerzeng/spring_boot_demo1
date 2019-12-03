11d36a52a954ca93e4dfb1dd4e5421d543477805
zengfanlong1 <zengfanlong1@globalegrow.com>
2019-12-03 14:33:49 +0800

    redis

---
+++ src/main/java/com/spring_boot_demo1/component/util/Redis.java
@@ -0,0 +1,47 @@
+package com.spring_boot_demo1.component.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+@SuppressWarnings("all")
+public class Redis {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    public void set(String key, String value) {
+        stringRedisTemplate.opsForValue().set(key, value);
+    }
+
+    public String get(String key) {
+        return stringRedisTemplate.opsForValue().get(key);
+    }
+
+    public long ttl(String key, TimeUnit timeUnit) {
+        return stringRedisTemplate.getExpire(key, timeUnit);
+    }
+
+    public boolean expire(String key, long timeout, TimeUnit timeUnit) {
+        return stringRedisTemplate.expire(key, timeout, timeUnit);
+    }
+
+    public String getSet(String key, String value) {
+        return stringRedisTemplate.opsForValue().getAndSet(key, value);
+    }
+
+    public boolean del(String key) {
+        return stringRedisTemplate.delete(key);
+    }
+
+    public void hset(String key, String field, Object value) {
+        stringRedisTemplate.opsForHash().put(key, field, value);
+    }
+
+    public void hget(String key, String field) {
+        stringRedisTemplate.opsForHash().get(key, field);
+    }
+}
---
+++ src/main/java/com/spring_boot_demo1/controller/RedisController.java
@@ -0,0 +1,35 @@
+package com.spring_boot_demo1.controller;
+
+import com.spring_boot_demo1.component.util.Redis;
+import com.spring_boot_demo1.mysql.entity.User;
+import com.spring_boot_demo1.mysql.service.UserService;
+import org.apache.tomcat.jni.Time;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController /*直接返回*/
+@Component
+@RequestMapping("redis")
+public class RedisController {
+
+    @Autowired
+    public Redis redis;
+
+    @RequestMapping("insert/{key}/{value}")
+    public void Insert(@PathVariable String key, @PathVariable String value) throws InterruptedException {
+        redis.set(key, value);
+        for (int i = 1; i < 1000; i++) {
+            Thread.sleep(100);
+            redis.hset(key + "_hset", String.valueOf(System.currentTimeMillis()), value);
+        }
+    }
+
+    @RequestMapping("get/{key}")
+    public String Insert(@PathVariable String key) {
+        return redis.get(key);
+    }
+
+}
