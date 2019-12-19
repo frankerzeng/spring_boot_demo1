930f645392e7c844763e9e5c5388d85c20d38cdc
frankerzeng <frankerzeng@163.com>
2019-12-19 21:35:33 +0800

    package

---
+++ src/main/java/com/spring_boot_demo1/controller/test/ListDemo.java
@@ -0,0 +1,17 @@
+package com.spring_boot_demo1.controller.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by fanlong on 2019/12/19.
+ */
+public class ListDemo {
+    public static void main(String[] args) {
+        ArrayList<Object> list = new ArrayList<Object>();
+        list.add(1);
+        list.add("d");
+        list.add('d');
+        System.out.print(list);
+    }
+}
--- src/main/java/com/spring_boot_demo1/controller/test/Thread.java
+++ src/main/java/com/spring_boot_demo1/controller/test/Thread.java
@@ -1,13 +1,10 @@
-package com.spring_boot_demo1.controller;
+package com.spring_boot_demo1.controller.test;
 
-public class test {
-}
-
-class ThreadB extends Thread {
+class Thread extends java.lang.Thread {
 
     private Object obj;
 
-    public ThreadB(String name) {
+    public Thread(String name) {
         super(name);
     }
 
@@ -18,7 +15,7 @@ class ThreadB extends Thread {
                     System.out.printf("%s: %d\n", this.getName(), i);
                     // i能被4整除时，休眠100毫秒
                     if (i % 4 == 0)
-                        Thread.sleep(100);
+                        java.lang.Thread.sleep(100);
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -27,8 +24,8 @@ class ThreadB extends Thread {
     }
 
     public static void main(String[] args) {
-        ThreadB t1 = new ThreadB("t1");
-        ThreadB t2 = new ThreadB("t2");
+        Thread t1 = new Thread("t1");
+        Thread t2 = new Thread("t2");
         t1.start();
         t2.start();
     }
